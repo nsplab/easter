85ad214c2ea54f8e259d78067ac43e3a913b4608
root <root@NSPLab4.(none)>
2014-04-17 15:53:46 -0700

    analysis code added

---
+++ analysis/ssvep/ssvep.m
@@ -0,0 +1,85 @@
+% sampling rate
+fs = 9600;
+windlengthSeconds = 2;
+windlength = windlengthSeconds * fs;
+noverlapPercent = 0.0;%windlength * 0.25;
+noverlap = windlength * noverlapPercent;
+
+% ask the user to select the data file
+%run('../loaddata.m');
+
+fid = fopen([pathname filename], 'r');
+digitalinCh = 65;
+fseek(fid, 4*(digitalinCh-1), 'bof');
+dataColumn = fread(fid, Inf, 'single', 4*64);
+time_axis = (0:length(dataColumn)-1)*1.0/fs;
+figure;plot(time_axis,dataColumn);
+
+tmp = (dataColumn>0);
+t1 = diff(tmp);
+t2 = find(t1==1);
+t3 = find(t1==-1);
+
+cutSecondsBegining = 10;
+cutSecondsEnd = 15;
+
+cutLengthB = fs * cutSecondsBegining;
+cutLengthE = fs * cutSecondsEnd;
+
+% choose the colormap for the plots
+CM = colorcube(size(data,1)+1);
+%CM = [hex2dec('e9'), hex2dec('00'), hex2dec('3a'); hex2dec('ff'), hex2dec('ba'),hex2dec('00'); hex2dec('18'),hex2dec('26'),hex2dec('b0'); hex2dec('58'),hex2dec('e0'), hex2dec('00')];
+%CM = CM/256;
+
+%scalpCh = [8,4,6,7];
+
+% the titles in the legend
+legends = [];
+
+
+dataname = 'SSVEP';
+
+% draw the figure with white background and full screen
+figure('Color',[1 1 1],'units','normalized','outerposition',[0 0 1 1]);
+hold on
+
+% compute PSD of all channels and plot the PSD + 95% confidence intervals
+for ii=1:size(data,1)
+    ii
+    [pxx, f] = pwelch(data{ii,2}((1+cutLengthB):(end-(cutLengthE))), (windlength), noverlap, windlength, fs);
+    %[pxx, f, pxxc] = pwelch(dataExp{scalpCh(i),2}((1+cutLengthB):(end-(cutLengthE))), (windlength), noverlap, windlength, fs, 'ConfidenceLevel', 0.95);
+    %[pxxC, fC, pxxcC] = pwelch(dataCtr{scalpCh(i),2}((1+cutLengthB):(end-(cutLengthE))), (windlength), noverlap, windlength, fs, 'ConfidenceLevel', 0.95);
+    
+    nump = 1000;    
+    f = f(1:nump);
+    pxx = pxx(1:nump);
+
+    %diffPow = log10(pxx) ./ log10(pxxC);
+    %diffPower(diffPower<0) = 0.00001;
+    
+    legends(end+1) = plot(f, log10(pxx),'color',CM(ii,:),'linewidth',3);
+    %plot(f,log10(pxxc),'color',CM(i,:),'linewidth',1);
+end
+
+% add the legend
+legend(legends, data{:,1})
+%legend(legends, {'left eye','right eye', 'middle head', 'back head'});
+
+% put a tick every 10 Hz
+%tmp = get(gca,'xlim');
+%set(gca,'XTick',[0:10:tmp(2)]);
+
+% set labels and grid
+xlabel('frequency');
+ylabel('${log}_{10} (\mu V^2/{Hz})$','Interpreter','LaTex');
+tmp = title(['Experiment PSD: ' dataname  '_' filename ' |  window length (secs)=' int2str(windlengthSeconds) '  overlap=' num2str(noverlapPercent)]);
+set(tmp,'interpreter','none');
+%grid on
+%grid minor
+%ylim([-7 6]);
+
+tmp = title(['SSVEP: '  allData{1}(i)]);
+set(tmp,'interpreter','none');
+
+saveas(fgh, ['matlab_data/' S{i} '.fig']);
+
---
+++ analysis/vep/plot_all_vep.m
@@ -0,0 +1,84 @@
+
+% read the comments wirtten on the experiment day
+% each line correspondes to an experiment
+fid = fopen('../data/neuro/vep.txt');
+allData = textscan(fid,'%s','Delimiter','\n');
+
+% get the list of data/experiment filenames
+% sorted based on the modification time
+tmp = dir('../data/neuro/vep');
+S = [tmp(:).datenum].';
+[S,S] = sort(S);
+S = {tmp(S).name};
+
+% generate a table whose 
+% first column holds the filename of the data
+% columns 2 to 11 hold the name/location of the channels
+% the last column holds the which channels was used as GND
+a = cell(length(S)-2,2);
+for i=1:length(S)-2,
+    a{i,1} = S(i);
+    a{i,2} = 'Disconnected';
+    a{i,3} = 'Endo';
+    a{i,4} = 'Mid head';
+    a{i,5} = 'Disconnected';
+    a{i,6} = 'Right Eye';
+    a{i,7} = 'Right Leg';
+    a{i,8} = 'Back Head';
+    a{i,9} = 'Left Eye';
+    a{i,10} = 'Top Precordial';
+    a{i,11} = 'Bottom Precordial';
+    a{i,12} = 'Nose';
+end
+
+all_experiments_channels = cell(size(a,1), size(a,2));
+all_experiments_channels(2:end+1,:) = a(1:end,:);
+all_experiments_channels(1,1) = {'filename'};
+all_experiments_channels(1,2:end-1) = {'channel name'};
+all_experiments_channels(1,end) = {'GND'};
+
+
+% iterate over the data filenames
+% 
+original_sampling_rate_in_Hz = 9600;
+decimate_factor = 10;
+decimated_sampling_rate_in_Hz = original_sampling_rate_in_Hz/decimate_factor;
+
+for i=1:(length(S)-2),
+    dontUseGUI = 1;
+    pathname = '../data/neuro/vep/';
+    S{i}
+    filename = S{i};
+    pwd
+    fid = fopen([pathname filename], 'r');
+    
+    original_data = cell(0,2);
+    data_decimated = cell(0,2);
+    
+    maxNumberOfChannels = 10;
+    for j=1:maxNumberOfChannels,
+        fseek(fid, 4*(j-1), 'bof');
+        dataColumn = fread(fid, Inf, 'single', 4*64);
+        channelName = a{i,j+1};
+        
+        original_data(end+1,1) = {channelName};
+        original_data(end,2) = {dataColumn};
+        
+        dataColumn_decimated = decimate(dataColumn, decimate_factor);
+        data_decimated(end+1,1) = {channelName};
+        data_decimated(end,2) = {dataColumn_decimated};
+
+    end
+    
+    GND = a{i,12};
+    data = original_data;
+    sampling_rate_in_Hz = original_sampling_rate_in_Hz;
+    save([S{i} '.mat'], 'data', 'sampling_rate_in_Hz', 'GND','all_experiments_channels');
+
+    data = data_decimated;
+    sampling_rate_in_Hz = decimated_sampling_rate_in_Hz;
+    save(['../data/neuro/matlab_data' S{i} '_decimated.mat'], 'data', 'sampling_rate_in_Hz', 'GND','all_experiments_channels');
+
+    data = original_data;
+    run('arduino_vep.m');
+end

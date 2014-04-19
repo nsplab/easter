42bfcc750981da30d57bcc8f8b78192a2f51118c
rsrinivasan02 <rsrinivasan02@hotmail.com>
2014-04-18 19:29:04 -0700

    vep analysis code rehauled and commented

--- analysis/vep/arduino_vep.m
+++ analysis/vep/arduino_vep.m
@@ -16,11 +16,6 @@
 
 
 %///// hardcoded values that you should keep unchanged
-fs = 9600;                                                                 %sampling rate is hardcoded at 9600 Hz; determined by recording code
-                                                                           %that controls g.hiamp (see /code/easter/recording/
-
-digitalinCh = 65;                                                          %designate the digital input channel, which is used to record
-                                                                           %the LED state to align EEG data with onset or offset to generate VEP graphs 
 
                                                                            %digital input channel convention is low is LED OFF, high is LED ON; verified for VEP aruino recording code with 5rabbit, 6rabbit, 7rabbit, etc.
 LED_ON_edge = 1;                                                           %LED turns on with rising edge of digital in channel (diff = 1)
@@ -37,9 +32,9 @@ postEventPlot_sec = 1/0.9/2;                                               %time
 
 
 % OPTION 2 /////// Choose which filters you want to use in the analysis                                                                            
-use_lpf = 1;                                                               %0 or 1; use the lowpass filter hardcoded in the lines below
-use_hpf = 1;                                                               %0 or 1; use the highpass filter hardcoded in the lines below
-use_nf_60_120 = 1;                                                         %0 or 1; use the 60 and 120 Hz notch filters
+use_lpf = 0;                                                               %0 or 1; use the lowpass filter hardcoded in the lines below
+use_hpf = 0;                                                               %0 or 1; use the highpass filter hardcoded in the lines below
+use_nf_60_120 = 0;                                                         %0 or 1; use the 60 and 120 Hz notch filters
 
 
 % OPTION 3 /////// Choose desired properties of the lowpass, highpass, and notch filter options
@@ -47,7 +42,7 @@ hp = fdesign.highpass('Fst,Fp,Ast,Ap',(90.0),(105.0),90,1,fs);             %high
 hpf = design(hp, 'butter');
 
 lp = fdesign.lowpass('Fp,Fst,Ap,Ast',(300.0),(350.0),1,90,fs);             %lowpass filter; passband 300 Hz, stopband 350 Hz, 1dB passband ripple, 90 dB stopband attenuation, sampling frequency fs, butterworth 
-lpf = design(lp2, 'butter');                                              
+lpf = design(lp, 'butter');                                              
 
 n60 = fdesign.notch('N,F0,Q,Ap',6,60,10,1,fs);                             %set parameters for 60 Hz notch filter  (N - filter order, F0 - center frequency, Q - quality factor, Ap - passband ripple (decibels)
 n120 = fdesign.notch('N,F0,Q,Ap',6,120,10,1,fs);                           %set parameters for 120 Hz notch filter (N - filter order, F0 - center frequency, Q - quality factor, Ap - passband ripple (decibels)
@@ -59,24 +54,12 @@ nf120 = design(n120);                                                      %impl
 
 
 
-%//////////////////////////////////
-% read the digital input channel, which contains the LED state
-% (in 7rabbit, this channel is low when LED is on, and high when LED is off;
-% in 6rabbit and 5rabbit, this channel is low when LED is off, and high when LED is on;
-% this convention is set by the arduino code in /code/easter/recording.
-% for experiments subsequent to 7rabbit, the arduino code was set back to the
-% intuitivfe convention of low when LED is off, and high when LED is on.
-fid = fopen([pathname filename], 'r');                                     %open the binary data file written in our easter binary format
-fseek(fid, 4*(digitalinCh-1), 'bof');                                      %fseek sets the pointer to the first value read in the file. 4 represents 4 bytes.
-                                                                           %we're starting to read from the digital input channel, the first value of which is
-                                                                           %4*(digitalinCh-1) bytes into the file.
-dataColumnDig = fread(fid, Inf, 'single', 4*64);                           %fread iteratively reads values the file and places them into the vector 'dataColumn', 
-                                                                           %representing all amplitudes from the current channel.. Inf causes fread to continue until it 
-                                                                           %reaches EOF (end of file). 4*64 tells fread to skip 4*64 bytes to get the next value.
-%time_axis = (0:length(dataColumnDig)-1)*1.0/fs;                           %define a time axis, based on sampling rate (fs) and the length of the recording
-%figure;plot(time_axis,dataColumnDig);                                     %plot and visualize the digital input channel
 
-cleanDigitalIn = (dataColumnDig>0);                                        %make sure the digital In takes on binary values: take any negative excursions to 0
+
+%//////////////////////////////////
+% Look for LED ON and OFF transitions based on digital input channel (65)
+%//////////////////////////////////
+
 diff_cleanDigitalIn = diff(cleanDigitalIn);                                %compute any edges in the digital trace
 event_index_LED_ON = find(diff_cleanDigitalIn==LED_ON_edge);               %find the edges that correspond to LED turning on
 event_index_LED_OFF = find(diff_cleanDigitalIn==LED_OFF_edge);             %find the edges that correspond to LED turning off
@@ -84,25 +67,33 @@ event_index_LED_OFF = find(diff_cleanDigitalIn==LED_OFF_edge);             %find
 
 preEventPlot_samples = floor(preEventPlot_sec*fs);                         %time in # samples to extend plot prior to event time; preEventPlot_sec - see hardcoded parameters above
 postEventPlot_samples = floor(postEventPlot_sec*fs)-1;                     %time in # samples to extend plot following event time; postEventPlot_sec - see hardcoded parameters above
-totalEventPlot_samples = preEventPlot_samples + postEventPlot_samples+1;     % total # of samples to plot
-time_axis = (-preEventPlot_samples:postEventPlot_samples)/fs*1000;          %prepare the time axis for the plots based on samples to plot
+totalEventPlot_samples = preEventPlot_samples + postEventPlot_samples+1;   % total # of samples to plot
+time_axis = (-preEventPlot_samples:postEventPlot_samples)/fs*1000;         %prepare the time axis for the plots based on samples to plot
 
-% choose the colormap for the plots
-CM = colorcube(size(data,1)+1);
-%CM = distinguishable_colors(size(data,1)+1);
 
-% draw the figure with white background and full screen
-fgh = figure('Color',[1 1 1],'units','normalized','outerposition',[0 0 1 1]);
-hold on
+fgh = figure('Color',[1 1 1],'units','normalized',...                      %get the figure handle, and draw the figure with white background and full screen
+                                                'outerposition',[0 0 1 1]);
+CM = colorcube(size(data,1)+1);                                            % choose the colormap for the plots
+hold on;
 
-plotline_handles = [];                                                      %prepare to accumulate plot line handles for the figure legend
-
+plotline_handles_ON = [];                                                  %prepare to accumulate LED ON plot line handles for the figure legend
+plotline_handles_OFF = [];                                                 %prepare to accumulate LED OFF plot line handles for the figure legend
+channels_plotted = [];                                                     %keep track of which channels were actually plotted - some are skipped if plot_disconnected_channels = 0 (set in plot_all_vep)
+                                                                           %used for the figure legends
     
-for ii=1:size(data,1)
-    ii
-    
-    chData = data{ii,2};
+for ii=1:size(data,1)                                                      %for each channel
 
+                                                                           %plot_only_neuro_and_endo_channels - set in plot_all_vep.m
+    if (plot_only_neuro_and_endo_channels) && ((strcmp(data{ii,1},'Disconnected') || strcmp(data{ii,1},'Top Precordial') || strcmp(data{ii,1},'Bottom Precordial') || strcmp(data{ii,1},'Right Leg') ))
+        disp(['Channel Skipped: ' int2str(ii)]);
+        continue;                                                          %tells Matlab to skip the rest of this for loop iteration and go to the next iteration
+    else
+        channels_plotted = [channels_plotted ii];
+    end
+        
+    
+    chData = data{ii,2};                                                   %load the timeseries recording for that channel
+    
     %//////////////////////////////////////////////////////////////////////
     %/// filter the data if it's been requested in hardcoded block above
     %//////////////////////////////////////////////////////////////////////
@@ -119,6 +110,11 @@ for ii=1:size(data,1)
     %//////////////////////////////////////////////////////////////////////
     
 
+    %//////////////////////////////////////////////////////////////////////
+    %/// compute and plot the average event-aligned LED ON response for
+    %this channel
+    %//////////////////////////////////////////////////////////////////////
+
     chData_all_single_trial_collection_ON = [];                            %matrix of all single trial responses for the single channel stored in chData, aligned to ON (rising edges of digital input signal)
     for jj=2:length(event_index_LED_ON)-1,                                 %for each LED ON event, snip out that segment and append it as a row in chData_all_single_trial_responses_ON
         chData_all_single_trial_collection_ON(end+1, 1:totalEventPlot_samples)...
@@ -126,33 +122,70 @@ for ii=1:size(data,1)
                             (event_index_LED_ON(jj)+postEventPlot_samples)));
     end
     chData_trial_averaged_ON = mean(chData_all_single_trial_collection_ON);%record the trial-averaged response aligned to LED ON (rising edges of digital input signal)
+
+    subplot(2,1,1);hold on;
+    plotline_handles_ON(end+1) = plot(time_axis, chData_trial_averaged_ON,'color',CM(ii,:),'linewidth',2);
     
+       
+    %//////////////////////////////////////////////////////////////////////
+    %/// compute and plot the average event-aligned LED OFF response for this
+    %channel
+    %//////////////////////////////////////////////////////////////////////
 
-
-    plotline_handles(end+1) = plot(time_axis, am,'color',CM(ii,:),'linewidth',2);
+    chData_all_single_trial_collection_OFF = [];                            %matrix of all single trial responses for the single channel stored in chData, aligned to OFF (rising edges of digital input signal)
+    for jj=2:length(event_index_LED_OFF)-1,                                 %for each LED OFF event, snip out that segment and append it as a row in chData_all_single_trial_responses_OFF
+        chData_all_single_trial_collection_OFF(end+1, 1:totalEventPlot_samples)...
+            = detrend(chData((event_index_LED_OFF(jj)-preEventPlot_samples):...%segment and detrend signal from this individual trial and channel
+                            (event_index_LED_OFF(jj)+postEventPlot_samples)));
+    end
+    chData_trial_averaged_OFF = mean(chData_all_single_trial_collection_OFF);%record the trial-averaged response aligned to LED ON (rising edges of digital input signal)
+    
+    subplot(2,1,2);hold on;
+    plotline_handles_OFF(end+1) = plot(time_axis, chData_trial_averaged_OFF,'color',CM(ii,:),'linewidth',2);
 end
 
-% add the legend
-legend(plotline_handles, data{:,1})
 
-tmp = title([ 'VEP: ' filename ' | GND: nose |  # samples =' int2str(length(t2)) ]);
-set(tmp,'interpreter','none');
 
-xlabel('time (ms)');
-ylabel('$\mu V$','Interpreter','LaTex');
 
+
+
+% add the legend and labels
+subplot(2,1,1);
+legend(plotline_handles_ON, data{channels_plotted,1})
 hold on
-plot([0 0], ylim, 'linewidth',3)
-
-grid on
-grid minor
-
-axis tight
-
-hold off
-
-tmp = title(['VEP: '  allData{1}(i)]);
+plot([0 0], ylim, 'linewidth',4)
+%tmp = title([ 'VEP ON Response: ' filename ' | GND: nose |  # samples =' int2str(length(t2)) ]);
+if ~isnan(allData)
+    tmp = title(['VEP ON Response: '  allData{1}(i)]);
+else
+    tmp = title(['VEP ON Response']);
+end
 set(tmp,'interpreter','none');
+xlabel('time (ms) relative to LED ON at time zero');
+ylabel('$\mu V$','Interpreter','LaTex');
+%grid on
+%grid minor
+%axis tight
+
+subplot(2,1,2);
+legend(plotline_handles_OFF, data{channels_plotted,1})
+hold on
+plot([0 0], ylim,'b--','linewidth',4)
+%tmp = title([ 'VEP OFF Response: ' filename ' | GND: nose |  # samples =' int2str(length(t2)) ]);
+if ~isnan(allData)
+    tmp = title(['VEP OFF Response: '  allData{1}(i)]);
+else
+    tmp = title(['VEP OFF Response']);
+end
+set(tmp,'interpreter','none');
+xlabel('time (ms) relative to LED OFF at time zero');
+ylabel('$\mu V$','Interpreter','LaTex');
+%grid on
+%grid minor
+%axis tight
+
+
+
 
 %saveas(fgh, ['matlab_data/' S{i} '.fig']);		%save the matlab figure to file
 
--- analysis/vep/plot_all_vep.m
+++ analysis/vep/plot_all_vep.m
@@ -19,25 +19,59 @@
 
 
 %///////////////////////////////////////////////
-% Hardcoded variables
+% START BLOCK: Hardcoded variables
 %///////////////////////////////////////////////
-rabbit_ID = '7rabbit_apr_15_2014';			%'7rabbit_apr_15_2014' corresponds to the experiment performed 4/15/14
-pathname = ['../../data/' rabbit_ID '/neuro/vep/'];	%path for the data in easter binary format
-pathname_comments = ['../../data/' rabbit_ID '/neuro/vep.txt'];%file containing comments written on the experiment day to use as labels for the figure titles
-							% read the comments written on the experiment day to use as labels for the figure titles
-							% each line correspondes to a single run of the VEP experiment
-							% this file needs to be prepared by hand
-							% the total # of lines should equal the number of VEP sessions
-							% the ordering of comments needs to be chronological
-							% example lines:
-							% 12:30 This is the comment about this particular session. g.tech Ground was nose.
-							% 12:45 This was the next condition. Used Faraday cage. g.tech Ground was right leg.
+rabbit_ID = '7rabbit_apr_15_2014';                                         %'7rabbit_apr_15_2014' corresponds to the experiment performed 4/15/14
 
-original_sampling_rate_in_Hz = 9600;			% data is acquired with the g.Tech g.HiAmp at 9600 Hz. this is fixed for convenience. at higher rates, the 
-							% digital input channel does not work reliably.
-decimate_factor = 10;					% this code also saves a decimated trace of the data (properly downsampled using anti-aliasing, with decimate.m).
 
+decimate_factor = 10;                                                      % decimate the data to speed up plotting (decimate_factor = 1 is no decimation; 10 is reasonable)
+                                                                           %(this Matlab function implements proper downsampling using anti-aliasing, with decimate.m).
+
+
+maxNumberOfChannels = 10;                                                  %# of consecutive analog input channels, starting with channel 1, to extract from the project easter binary files
+                                                                           %(which contain 64 analog channels and the digital in channel (channel 65)
+
+digitalinCh = 65;                                                          %designate the digital input channel, which is used to record
+                                                                           %the LED state to align EEG data with onset or offset to generate VEP graphs 
+
+original_sampling_rate_in_Hz = 9600;                                       % data is acquired with the g.Tech g.HiAmp at 9600 Hz. this is fixed for convenience. at higher rates, the 
+                                                                           % digital input channel does not work reliably.
+
+pathname = ['../../../../data/easter/' rabbit_ID '/neuro/binary_data/vep/'];%path for the data in easter binary format
+pathname_comments = ['../../../../data/easter/' rabbit_ID '/neuro/vep.txt'];%file containing comments written on the experiment day to use as labels for the figure titles
+                                                                           % read the comments written on the experiment day to use as labels for the figure titles
+                                                                           % each line correspondes to a single run of the VEP experiment
+                                                                           % this file needs to be prepared by hand
+                                                                           % the total # of lines should equal the number of VEP sessions
+                                                                           % the ordering of comments needs to be chronological
+                                                                           % example lines:
+                                                                           % 12:30 This is the comment about this particular session. g.tech Ground was nose.
+                                                                           % 12:45 This was the next condition. Used Faraday cage. g.tech Ground was right leg.
+
+%Channel labels during VEP for all rabbits (5,6,7, etc.)
+switch rabbit_ID
+    case '7rabbit_apr_15_2014'
+        channelNames_VEP = {'Disconnected','Endo','Mid head','Disconnected','Right Eye','Right Leg','Back Head','Left Eye','Bottom Precordial','Top Precordial'};
+        plot_only_neuro_and_endo_channels = 0;                             % choose to plot only neuro and endo channels, excluding disconnected or precordial channels
+        gtechGND = 'Nose';
+        earth = 'Left Leg';
+    case '6rabbit_apr_15_2014'
+        channelNames_VEP = {'Disconnected','Endo','Mid head','Disconnected','Right Eye','Right Leg','Back Head','Left Eye','Bottom Precordial','Top Precordial'};
+    case '5rabbit_apr_15_2014'
+%        channelNames_VEP = 
+end
 %///////////////////////////////////////////////
+% END BLOCK: Hardcoded variables
+%///////////////////////////////////////////////
+
+
+
+tmp = dir(pathname);                                                       % get the list of data filenames for this particular type of evoked potential
+S = {tmp(3:end).name};                                                     % chop out the '.' and '..' filename entries that are in every directory
+%                                                                          %note from Ram, 4/18/14: do *not* sort by modification time - this may change; instead, rely on lexicographic sort of name which contains timestamp
+%S = [tmp(:).datenum].';                                                   
+%[S,S] = sort(S);
+%S = {tmp(S).name};                                                        
 
 
 
@@ -51,12 +85,68 @@ end
 
 
 %////////////////////////////////////////////////////////////////////////////////////////
+% START BLOCK: Extract Data from Project Easter Binary Files to Prep for
+% function call to arduino_vep.m
+%////////////////////////////////////////////////////////////////////////////////////////
+
+for i=1:length(S),				%for each data file in the directory
+    filename = S{i};
+    fid = fopen([pathname filename], 'r');
+    
+    data = cell(0,2);
+
+    for j=1:maxNumberOfChannels,                                           %for the jth analog channel (excluding the digital in channel), load the signal into a vector in original_data{j,2}
+        fseek(fid, 4*(j-1), 'bof');
+        dataColumn = fread(fid, Inf, 'single', 4*64);
+        channelName = channelNames_VEP{j};
+        
+        dataColumn = decimate(dataColumn,decimate_factor);                 %decimate the data by the requested factor for speed
+        data(end+1,1) = {channelName};
+        data(end,2) = {dataColumn};
+        
+    end
+    
+%    save([S{i} '.mat'], 'data', 'sampling_rate_in_Hz', 'GND','all_experiments_channels');
+end
+GND = gtechGND;                                                            %label indicating position of gtech ground (differential input to the g.HiAmp)
+fs = original_sampling_rate_in_Hz/decimate_factor;                         %variable name 'fs' is used by arduino_vep.m
+
+
+%//////////////////////////////////
+% Load the Digital Input Channel (65)
+%//////////////////////////////////
+% read the digital input channel, which contains the LED state
+% (in 7rabbit, this channel is low when LED is on, and high when LED is off;
+% in 6rabbit and 5rabbit, this channel is low when LED is off, and high when LED is on;
+% this convention is set by the arduino code in /code/easter/recording.
+% for experiments subsequent to 7rabbit, the arduino code was set back to the
+% intuitive convention of low when LED is off, and high when LED is on.
+fid = fopen([pathname filename], 'r');                                     %open the binary data file written in our easter binary format
+fseek(fid, 4*(digitalinCh-1), 'bof');                                      %fseek sets the pointer to the first value read in the file. 4 represents 4 bytes.
+                                                                           %we're starting to read from the digital input channel, the first value of which is
+                                                                           %4*(digitalinCh-1) bytes into the file.
+dataColumnDig = fread(fid, Inf, 'single', 4*64);                           %fread iteratively reads values the file and places them into the vector 'dataColumn', 
+                                                                           %representing all amplitudes from the current channel.. Inf causes fread to continue until it 
+                                                                           %reaches EOF (end of file). 4*64 tells fread to skip 4*64 bytes to get the next value (assumes 65 channels recorded)
+dataColumnDig = dataColumnDig(1:decimate_factor:end);                      %throw out intermediate samples to keep them aligned to the decimated channel data
+cleanDigitalIn = (dataColumnDig>0);                                        %make sure the digital In takes on binary values: take any negative excursions to 0
+%time_axis = (0:length(dataColumnDig)-1)*1.0/fs;                           %define a time axis, based on sampling rate (fs) and the length of the recording
+%figure;plot(time_axis,dataColumnDig);                                     %plot and visualize the digital input channel
+%//////////////////////////////////
+%//////////////////////////////////////////////////////////
+% END BLOCK: Extract Data from Project Easter Binary Files
+%//////////////////////////////////////////////////////////
+
+
+return;
+
+
+
+%////////////////////////////////////////////////////////////////////////////////////////
 % START BLOCK: Create Stimulus-Locked Figures, one for each data file in the directory
 %////////////////////////////////////////////////////////////////////////////////////////
 
-for i=1:(length(S)-2),				%for each data file in the directory
-    filename = S{i};
-    data = original_data;
+for i=1:length(S)				%for each data file in the directory (sorted by name which contains timestamp and listed in variable S)
     run('arduino_vep.m');
 end
 %///////////////////////////////////////////////
@@ -64,6 +154,8 @@ end
 %///////////////////////////////////////////////
 
 
+
+%{
 
 
 
@@ -73,14 +165,10 @@ end
 % electrode positions for each channel, in every run that was recorded
 %///////////////////////////////////////////////
 
-% get the list of data filenames for this particular type of evoked potential
-% sorted based on the modification time
-tmp = dir(pathname);
-S = [tmp(:).datenum].';
-[S,S] = sort(S);
-S = {tmp(S).name};
+
 
 % generate a structure that documents the electrode positions for each channel, in every run that was recorded
+% row index:   filename index
 % column 1:	   filename of the data
 % columns 2 to 11: name/position of the channels (example: 'Mid head')
 % last column: 	   which channels was used as GND
@@ -116,7 +204,7 @@ all_experiments_channels(1,end) = {'GND'};
 
 decimated_sampling_rate_in_Hz = original_sampling_rate_in_Hz/decimate_factor;
 
-for i=1:(length(S)-2),				%for each data file in the directory, decimate and save
+for i=1:length(S),				%for each data file in the directory, decimate and save
     dontUseGUI = 1;
     S{i}
     filename = S{i};
@@ -144,11 +232,11 @@ for i=1:(length(S)-2),				%for each data file in the directory, decimate and sav
     GND = a{i,12};
     data = original_data;
     sampling_rate_in_Hz = original_sampling_rate_in_Hz;
-    save([S{i} '.mat'], 'data', 'sampling_rate_in_Hz', 'GND','all_experiments_channels');
+%    save([S{i} '.mat'], 'data', 'sampling_rate_in_Hz', 'GND','all_experiments_channels');
 
     data = data_decimated;
     sampling_rate_in_Hz = decimated_sampling_rate_in_Hz;
-    save(['../data/neuro/matlab_data' S{i} '_decimated.mat'], 'data', 'sampling_rate_in_Hz', 'GND','all_experiments_channels');
+%    save(['../data/neuro/matlab_data' S{i} '_decimated.mat'], 'data', 'sampling_rate_in_Hz', 'GND','all_experiments_channels');
 
 %    data = original_data;
 %    run('arduino_vep.m');
@@ -157,3 +245,5 @@ end
 % END BLOCK
 %///////////////////////////////////////////////
 
+
+%}

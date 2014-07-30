0f80a740b21b55a8c3447c25722f83fc48421d28
Bryan He <bryanhe@caltech.edu>
2014-07-29 21:09:50 -0700

    Code to plot qrs complex

---
+++ analysis/qrs/main.m
@@ -0,0 +1,190 @@
+clear;
+close all;
+
+addpath('../utilities');
+
+%% Constants
+channelNames_VEP = {'Disconnected','Endo','Mid head','Disconnected','Right Eye','Right Leg','Back Head','Left Eye','Bottom Precordial','Top Precordial'};
+maxNumberOfChannels = 10;
+digitalinCh = 65;
+channelToPlot = [2,3,5,7,8];
+%channelToPlot = [2];
+fs = 9600;
+
+CM = [hex2dec('e9'), hex2dec('00'), hex2dec('3a');
+      hex2dec('ff'), hex2dec('ba'), hex2dec('00');
+      hex2dec('40'), hex2dec('40'), hex2dec('ff');
+      hex2dec('58'), hex2dec('e0'), hex2dec('00');
+      hex2dec('b0'), hex2dec('00'), hex2dec('b0')];
+
+CM = CM/256;
+
+
+%% Filters
+hp = fdesign.highpass('Fst,Fp,Ast,Ap',(2.0),(10.0),90,1,fs);
+hpf = design(hp, 'butter');
+
+lp = fdesign.lowpass('Fp,Fst,Ap,Ast',(220.0),(256.0),1,90,fs);
+lpf = design(lp, 'butter');
+
+n60 = fdesign.notch('N,F0,BW,Ap',6,60,20,2,fs);
+n120 = fdesign.notch('N,F0,Q,Ap',6,120,10,1,fs);
+n180 = fdesign.notch('N,F0,Q,Ap',6,180,10,1,fs);
+nf60 = design(n60);
+nf120 = design(n120);
+nf180 = design(n180);
+
+for rabbit_ID = {'9rabbit_may_6_2014', '10rabbit_may_15_2014'}
+    rabbit_ID = rabbit_ID{1};
+    fprintf('rabbit_ID: %s\n', rabbit_ID);
+
+    switch rabbit_ID
+        case '9rabbit_may_6_2014'
+            vep_indices = [10 2 10 11 12 13 3];
+            ssvep_indices = [5 29 32 38 8];
+            ssaep_indices = [8 32 35 41 11];
+        case '10rabbit_may_15_2014'
+            vep_indices = [4 10 12 15 5];
+            ssvep_indices = [11 23 32 35 14];
+            ssaep_indices = [15 24 33 36 12];
+    end
+
+    pathname_comments = ['../../../../data/easter/' rabbit_ID '/neuro/neuro_experiment_log.txt'];
+
+    fid = fopen(pathname_comments);
+    if fid == -1
+        disp('Hey - you may want to create vep.txt to populate the titles in these figures. See plot_all_vep.m for details.');
+       return;
+    end
+
+    neuro_experiment_log = textscan(fid,'%s','Delimiter','\n');
+
+
+    for experiment = {'vep', 'ssvep', 'ssaep'}
+        experiment = experiment{1};
+        fprintf('experiment: %s\n', experiment);
+
+        pathname = ['../../../../data/easter/' rabbit_ID '/neuro/binary_data/' experiment '/'];
+        pathname_matdata = ['../../../../data/easter/' rabbit_ID '/neuro/matlab_data/' experiment '/'];
+
+        C = strfind(neuro_experiment_log{1}, ['- ' upper(experiment)]); % get rows with '- VEP' in them
+        rows = find(~cellfun('isempty', C));
+        allData = neuro_experiment_log{1}(rows);
+
+        tmp = dir(pathname);
+        S = {tmp(3:end).name}; 
+
+        %% Check for matching experiment log and data files
+        assert(length(S) == length(allData)); % check that the experiment log and data files have the same number of VEP runs
+        for i = 1:length(S)
+            % Hard coded parsing of file name
+            assert(strcmp(S{i}(1), '_'))
+            S_day = S{i}(2:4);
+            assert(strcmp(S{i}(5), '_'))
+            S_date = S{i}(6:7);
+            assert(strcmp(S{i}(8), '.'))
+            S_month = S{i}(9:10);
+            assert(strcmp(S{i}(11), '.'))
+            S_month = S{i}(12:15);
+            assert(strcmp(S{i}(16), '_'))
+            S_hour = S{i}(17:18);
+            assert(strcmp(S{i}(19:21), '%3A'))
+            S_minute = S{i}(22:23);
+            assert(strcmp(S{i}(24:26), '%3A'))
+            S_second = S{i}(27:28);
+            assert(strcmp(S{i}(29 + (0:numel(experiment)+1)), ['_' experiment '_']))
+            S_extra = S{i}(29+numel(experiment)+2:end);
+        
+            % Hard coded parsing of experiment log
+            header = allData{i};
+            if header(2) == ':'
+                header = ['0' header]; % one digit hour - pad with extra 0
+            end
+            header_hour = header(1:2);
+            assert(strcmp(header(3), ':'))
+            header_minute = header(4:5);
+            assert(strcmp(header(6), ':'))
+            header_second = header(7:8);
+        
+            % Check for explicit match
+            assert(strcmp(S_hour, header_hour));
+            assert(strcmp(S_minute, header_minute));
+            assert(strcmp(S_second, header_second));
+        end
+
+        indices = eval([experiment '_indices']);
+        for i_ = 1:numel(indices)
+            i = indices(i_);
+            filename = S{i};
+
+            % Clean name format
+            name = S{i};
+            name(name == '.') = '_';
+            while any(name == '%')
+                index = find(name == '%', 1);
+                name = [name(1:(index-1)) '_' name((index+3):end)];
+            end
+
+            fprintf('i_: %d / %d,\ti: %d,\t%s, \t%s\n', i_, numel(indices), i, filename, name);
+
+            %% Load data
+            fid = fopen([pathname filename], 'r');
+
+            data = cell(maxNumberOfChannels, 2);
+
+            for j=1:maxNumberOfChannels
+                fseek(fid, 4*(j-1), 'bof');
+                dataColumn = fread(fid, Inf, 'single', 4*64);
+                channelName = channelNames_VEP{j};
+
+                data(j,1) = {channelName};
+                data(j,2) = {dataColumn};
+            end
+
+            fseek(fid, 4*(digitalinCh-1), 'bof');
+            dataColumnDig = fread(fid, Inf, 'single', 4*64);
+            cleanDigitalIn = (dataColumnDig>0);
+
+            cardiacData = data{find(strcmp(data, 'Bottom Precordial')), 2};
+
+            cardiacData = filtfilt(nf60.sosMatrix, nf60.ScaleValues, cardiacData);
+            cardiacData = filtfilt(nf120.sosMatrix, nf120.ScaleValues, cardiacData);
+            cardiacData = filtfilt(nf180.sosMatrix, nf180.ScaleValues, cardiacData);
+
+            cardiacData = filtfilt(hpf.sosMatrix, hpf.ScaleValues, cardiacData);
+
+            cardiacData = filtfilt(lpf.sosMatrix, lpf.ScaleValues, cardiacData);
+
+            f1 = figure('units', 'pixels', 'outerposition', [0 0 1366 768]);
+            f2 = figure('units', 'pixels', 'outerposition', [0 0 1366 768]);
+            f3 = figure('units', 'pixels', 'outerposition', [0 0 1366 768]);
+            f4 = figure('units', 'pixels', 'outerposition', [0 0 1366 768]);
+            f5 = figure('units', 'pixels', 'outerposition', [0 0 1366 768]);
+
+            for ii=1:length(channelToPlot)
+                fprintf('ii: %d, %d\n', ii, channelToPlot(ii));
+
+                chData = data{channelToPlot(ii),2};
+
+                % Apply filters
+                chData = filtfilt(nf60.sosMatrix, nf60.ScaleValues,chData);
+                chData = filtfilt(nf120.sosMatrix, nf120.ScaleValues,chData);
+                chData = filtfilt(nf180.sosMatrix, nf180.ScaleValues,chData);
+
+                chData = filtfilt(hpf.sosMatrix, hpf.ScaleValues,chData);
+
+                chData = filtfilt(lpf.sosMatrix, lpf.ScaleValues,chData);
+
+                qrs_plot(chData, cardiacData, name, CM(ii, :), 0, 0, 0, f4, f5);
+
+            end
+
+            qrs_plot(0.2 * cardiacData, cardiacData, name, 'black', f1, f2, f3, f4, f5);
+
+            ansdkasnd
+            close all;
+
+        end
+    end
+end
+
---
+++ analysis/qrs/qrs_plot.m
@@ -0,0 +1,151 @@
+function [] = qrs_plot(chData, cardiacData, name, color, f1, f2, f3, f4, f5)
+
+    % chData must be at least high pass filtered (to get rid of drifting
+    % cardiacData probably has to be unfiltered to work
+    assert(all(size(chData) == size(cardiacData)));
+    %tic
+    [pks,locs]=findpeaks(cardiacData,'minpeakdistance', round(0.18 * 9600));
+    %toc
+    %locs = locs(2:(end-1));
+
+    if (nargin >= 5 && f1 ~= 0)
+        %% Plot Cardiac Data with Detected Peaks
+        figure(f1);
+        hold on;
+        plot((1:numel(cardiacData)) / 9600, cardiacData, 'color', color);
+        yl = ylim;
+        for i = 1:numel(locs)
+            plot(locs(i) * [1 1] / 9600, yl, 'color', 'black');
+        end
+        plot((1:numel(cardiacData)) / 9600, cardiacData, 'color', color);
+        ylim(yl);
+        xlabel('Time (seconds)');
+        ylabel('$\mu V$', 'interpreter', 'LaTeX');
+        title('Detected R Wave');
+        xlim([0 numel(cardiacData) / 9600]);
+    
+        %saveas(f1, ['matlab_data/' name '_' int2str(channel) '_r_peak.fig']);
+        saveas(f1, ['matlab_data/' name '_r_peak.fig']);
+    end
+
+    if (nargin >= 6 && f2 ~= 0)
+        %% Compute Distance between R Peaks
+        dt = diff(locs);
+    
+        %% Plot Distance between r peaks
+        figure(f2);
+        scatter(1:numel(dt), dt / 9600 * 1000, 36, color);
+        xlabel('Interval Number');
+        ylabel('Interval Length (ms)');
+        title('Time Between R Peaks');
+        xlim([0 numel(dt)]);
+        ylim([150 300]);
+    
+        %[a,b] = min(dt);
+        %figure(f1);
+        %xlim(locs(b) / 9600 + [-0.4 0.4]);
+        %locs(b + (-1:1)) / 9600 * 1000
+    
+        %saveas(f1, ['matlab_data/' name '_' int2str(channel) '_dt.fig']);
+        %save2pdf(['matlab_data/' name '_' int2str(channel) '_dt.pdf'], f2, 1200);
+        saveas(f2, ['matlab_data/' name '_dt.fig']);
+        save2pdf(['matlab_data/' name '_dt.pdf'], f2, 1200);
+    end
+
+    %% Compute interval (include first, exclude last)
+    cutoff = [1; locs(1:(end-1)) + round(diff(locs) / 2); numel(chData) + 1];
+
+    if (nargin >= 7 && f3 ~= 0)
+        %% Plot Distance between midpoints
+        figure(f3);
+        scatter(1:numel(dt), dt / 9600 * 1000, 36, color);
+        xlabel('Interval Number');
+        ylabel('Interval Length (ms)');
+        title('Time Between R Peaks');
+        xlim([0 numel(dt)]);
+        ylim([150 300]);
+    
+        %[a,b] = min(dt);
+        %figure(f1);
+        %xlim(locs(b) / 9600 + [-0.4 0.4]);
+        %locs(b + (-1:1)) / 9600 * 1000
+    
+        %saveas(f1, ['matlab_data/' name '_' int2str(channel) '_dt.fig']);
+        %save2pdf(['matlab_data/' name '_' int2str(channel) '_dt.pdf'], f3, 1200);
+        saveas(f3, ['matlab_data/' name '_dt.fig']);
+        save2pdf(['matlab_data/' name '_dt.pdf'], f3, 1200);
+    end
+
+    %% Compute start, R peak, end, and length of each interval
+    r_peak = locs;
+    start = cutoff(1:(end-1));
+    finish = cutoff(2:end);
+
+    assert(all(size(start) == size(r_peak)));
+    assert(all(size(start) == size(finish)));
+    assert(all(start < r_peak));
+    assert(all(r_peak < finish));
+
+    dt = finish - start;
+
+    %% Gather and align the channel data
+    %padded = nan(numel(r_peak), max(dt));
+    padded = zeros(numel(r_peak), max(dt));
+    for i = 1:numel(r_peak)
+        padded(i, 1:(finish(i)-start(i))) = chData(start(i):(finish(i)-1));
+    end
+
+    %% Check that every column/row has a non-nan value
+    assert(all(any(~isnan(padded),1)));
+    assert(all(any(~isnan(padded),2)));
+
+    %% Remove trials that have extreme values
+    invalid = any(abs(padded) > 1e3, 2); % for some reason after applying hpf/lpf/nf, first and last area have extreme values
+    num_invalid = sum(invalid)
+    assert(sum(invalid) < 15);
+    padded = padded(~invalid, :);
+
+    qrs = nanmean(padded, 1)';
+
+    if (nargin >= 8 && f4 ~= 0)
+        %% Plot Mean and Confidence Interval
+        figure(f4);
+        hold on;
+        plot((1:numel(qrs)) / 9600, qrs, 'color', color, 'linewidth', 2);
+        confMean = bootci(100, @nanmean, padded);
+        plot((1:numel(qrs)) / 9600, confMean(1,:),'--','color',color,'linewidth',2);
+        plot((1:numel(qrs)) / 9600, confMean(2,:),'--','color',color,'linewidth',2);
+    
+        xlabel('Time (seconds)');
+        ylabel('$\mu V$', 'interpreter', 'LaTeX');
+        title('QRS Complex Shape');
+        xlim([0 numel(qrs) / 9600]);
+        ylim([-100 100]);
+        set(findall(f4,'type','text'),'fontSize',40,'fontWeight','normal', 'color', [0,0,0]);
+        set(gca,'FontSize',40);
+    
+        %save2pdf(['matlab_data/' name '_' int2str(channel) '_qrs.pdf'], f4, 1200);
+        save2pdf(['matlab_data/' name '_qrs.pdf'], f4, 1200);
+    end
+
+    if (nargin >= 9 && f5 ~= 0)
+        %% Plot all curves
+        figure(f5);
+        plot((1:numel(qrs)) / 9600, padded, 'color', color);
+        xlabel('Time (seconds)');
+        ylabel('$\mu V$', 'interpreter', 'LaTeX');
+        title('QRS Complex Shape');
+        xlim([0 numel(qrs) / 9600]);
+        ylim([-100 100]);
+    
+        %save2pdf(['matlab_data/' name '_' int2str(channel) '_qrs_all.pdf'], f3, 1200);
+        save2pdf(['matlab_data/' name '_qrs_all.pdf'], f5, 1200);
+    end
+
+    %% Remove qrs
+    %filtered_data = zeros(size(chData));
+    %for i = 1:numel(r_peak)
+    %    filtered_data(start(i):(finish(i)-1)) = chData(start(i):(finish(i)-1)) - padded(i, 1:(finish(i)-start(i)))';
+    %end
+end
+
